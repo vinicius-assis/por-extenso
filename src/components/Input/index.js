13ec44c6c7a4b49130100ddcd7417e2a4424a854
Vinicius <viniciusassis.dev@hotmail.com>
2020-09-09 20:54:38 -0300

    Create input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Label, Field } from './style'
+
+
+
+const Input = () => (
+  <Label>
+    Valor
+    <Field />
+  </Label>
+)
+
+export default Input
---
+++ src/components/Input/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Label = styled.label`
+  font-size: 24px;
+  text-align: center;
+
+  display: flex;
+  flex-direction: column;
+`
+
+export const Field = styled.input`
+  height: 45px;
+  margin-top: 10px;
+`
