e1dd6cc1fae699def7caacb6258b4bf4d078f2a1
Vinicius <viniciusassis.dev@hotmail.com>
2020-09-10 16:55:34 -0300

    Add text align and padding in display component

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 const Display = styled.span`
   font-size: 2rem;
   font-weight: bold;
+  text-align: center;
   margin-top: 30px;
+  padding: 0 1rem;
 `
 
 export default Display
